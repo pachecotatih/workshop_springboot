251ab59664234bafcdee6fe75d65e3a99eba09a1
Tatiana Pacheco Barreto <tatianapacheco09@gmail.com>
2026-06-19 16:29:45 -0300

    Exception Update User

--- src/main/java/com/tatianapacheco/workshop_springboot/services/UserService.java
+++ src/main/java/com/tatianapacheco/workshop_springboot/services/UserService.java
@@ -12,6 +12,8 @@ import com.tatianapacheco.workshop_springboot.repositories.UserRepository;
 import com.tatianapacheco.workshop_springboot.services.exceptions.DatabaseException;
 import com.tatianapacheco.workshop_springboot.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -43,9 +45,14 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+
     }
 
     private void updateData(User entity, User obj) {
