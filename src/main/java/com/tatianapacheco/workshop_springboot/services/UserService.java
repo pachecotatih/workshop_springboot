9bff221d8db8650213aa61912f9d9664fd4d07dd
Tatiana Pacheco Barreto <tatianapacheco09@gmail.com>
2026-06-19 10:25:34 -0300

    Service layer, component registration

---
+++ src/main/java/com/tatianapacheco/workshop_springboot/services/UserService.java
@@ -0,0 +1,27 @@
+package com.tatianapacheco.workshop_springboot.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tatianapacheco.workshop_springboot.entities.User;
+import com.tatianapacheco.workshop_springboot.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+
+    }
+}
