bee55d0b74e1bd6131b954716c2e03241c6e4f72
Tatiana Pacheco Barreto <tatianapacheco09@gmail.com>
2026-06-19 15:36:38 -0300

    Payment, one-to-one association

--- src/main/java/com/tatianapacheco/workshop_springboot/config/TestConfig.java
+++ src/main/java/com/tatianapacheco/workshop_springboot/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.tatianapacheco.workshop_springboot.entities.Category;
 import com.tatianapacheco.workshop_springboot.entities.Order;
 import com.tatianapacheco.workshop_springboot.entities.OrderItem;
+import com.tatianapacheco.workshop_springboot.entities.Payment;
 import com.tatianapacheco.workshop_springboot.entities.Product;
 import com.tatianapacheco.workshop_springboot.entities.User;
 import com.tatianapacheco.workshop_springboot.entities.enums.OrderStatus;
@@ -79,6 +80,11 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
 
     }
 
